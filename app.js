4fc00b0e1fd5d4fc43dbc6fba8b51c5f84b6fab4
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-04-14 18:11:19 +0800

    Rewrite with ali-oss wrapper

--- app.js
+++ app.js
@@ -1,104 +1,112 @@
 'use strict';
 
-var appServer = '<your STS app server>';
-var bucket = 'your bucket name';
+var appServer = 'http://localhost:3000';
+var bucket = '<bucket-name>';
 var region = 'oss-cn-hangzhou';
 
-var applyToken = function* () {
+var urllib = OSS.urllib;
+var Buffer = OSS.Buffer;
+var OSS = OSS.Wrapper;
+var STS = OSS.STS;
+
+// Play without STS. NOT SAFE! Because access key id/secret are
+// exposed in web page.
+
+// var client = new OSS({
+//   region: 'oss-cn-hangzhou',
+//   accessKeyId: '<access-key-id>',
+//   accessKeySecret: '<access-key-secret>',
+//   bucket: '<bucket-name>'
+// });
+
+var applyTokenDo = function (func) {
   var url = appServer;
-  var result = yield OSS.urllib.requestThunk(url, {
+  return urllib.request(url, {
     method: 'GET'
-  });
-  return JSON.parse(result.data);
-};
-
-var progress = function* (p) {
-  var bar = document.getElementById('progress-bar');
-  bar.style.width = Math.floor(p * 100) + '%';
-  bar.innerHTML = Math.floor(p * 100) + '%';
-};
-
-var withStore = function (func) {
-  return function () {
-    OSS.co(function* () {
-      var creds = yield applyToken();
-      var store = new OSS({
-        region: region,
-        accessKeyId: creds.AccessKeyId,
-        accessKeySecret: creds.AccessKeySecret,
-        stsToken: creds.Security,
-        bucket: bucket
-      });
-
-      var result = yield func(store);
-
-      console.log(result);
-    }).catch(function (err) {
-      console.log(err);
+  }).then(function (result) {
+    var creds = JSON.parse(result.data);
+    var client = new OSS({
+      region: region,
+      accessKeyId: creds.AccessKeyId,
+      accessKeySecret: creds.AccessKeySecret,
+      stsToken: creds.SecurityToken,
+      bucket: bucket
     });
-  };
+
+    return func(client);
+  });
 };
 
-var uploadFile = function* (store) {
+var progress = function (p) {
+  return function (done) {
+    var bar = document.getElementById('progress-bar');
+    bar.style.width = Math.floor(p * 100) + '%';
+    bar.innerHTML = Math.floor(p * 100) + '%';
+    done();
+  }
+};
+
+var uploadFile = function (client) {
   var file = document.getElementById('file').files[0];
   var key = document.getElementById('object-key-file').value.trim() || 'object';
   console.log(file.name + ' => ' + key);
 
-  var result = yield store.multipartUpload(key, file, {progress: progress});
-  yield listFiles(store);
-
-  return result;
+  return client.multipartUpload(key, file, {
+    progress: progress
+  }).then(function (res) {
+    console.log('upload success: %j', res);
+    return listFiles(client);
+  });
 };
 
-var uploadContent = function* (store) {
+var uploadContent = function (client) {
   var content = document.getElementById('file-content').value.trim();
   var key = document.getElementById('object-key-content').value.trim() || 'object';
   console.log('content => ' + key);
 
-  var result = yield store.put(key, new OSS.Buffer(content));
-  yield listFiles(store);
-
-  return result;
+  return client.put(key, new Buffer(content)).then(function (res) {
+    return listFiles(client);
+  });
 };
 
-var listFiles = function* (store) {
+var listFiles = function (client) {
   var table = document.getElementById('list-files-table');
   console.log('list files');
 
-  var result = yield store.list({
+  return client.list({
     'max-keys': 100
+  }).then(function (result) {
+    var objects = result.objects.sort(function (a, b) {
+      var ta = new Date(a.lastModified);
+      var tb = new Date(b.lastModified);
+      if (ta > tb) return -1;
+      if (ta < tb) return 1;
+      return 0;
+    });
+
+    var numRows = table.rows.length;
+    for (var i = 1; i < numRows; i ++) {
+      table.deleteRow(table.rows.length - 1);
+    }
+
+    for (var i = 0; i < Math.min(3, objects.length); i ++) {
+      var row = table.insertRow(table.rows.length);
+      row.insertCell(0).innerHTML = objects[i].name;
+      row.insertCell(1).innerHTML = objects[i].size;
+      row.insertCell(2).innerHTML = objects[i].lastModified;
+    }
   });
-
-  var objects = result.objects.sort(function (a, b) {
-    var ta = new Date(a.lastModified);
-    var tb = new Date(b.lastModified);
-    if (ta > tb) return -1;
-    if (ta < tb) return 1;
-    return 0;
-  });
-
-  var numRows = table.rows.length;
-  for (var i = 1; i < numRows; i ++) {
-    table.deleteRow(table.rows.length - 1);
-  }
-
-  for (var i = 0; i < Math.min(3, objects.length); i ++) {
-    var row = table.insertRow(table.rows.length);
-    row.insertCell(0).innerHTML = objects[i].name;
-    row.insertCell(1).innerHTML = objects[i].size;
-    row.insertCell(2).innerHTML = objects[i].lastModified;
-  }
-
-  return result;
 };
 
-var downloadFile = function* (store) {
+var downloadFile = function (client) {
   var object = document.getElementById('dl-object-key').value.trim();
   var filename = document.getElementById('dl-file-name').value.trim();
   console.log(object + ' => ' + filename);
 
-  var result = store.signatureUrl(object, {
-    'content-disposition': 'attachment; filename="' + filename + '"'
+  var result = client.signatureUrl(object, {
+    response: {
+      'content-disposition': 'attachment; filename="' + filename + '"'
+    }
   });
   window.location = result;
 
@@ -106,10 +114,21 @@ var downloadFile = function* (store) {
 };
 
 window.onload = function () {
-  document.getElementById('file-button').onclick = withStore(uploadFile);
-  document.getElementById('content-button').onclick = withStore(uploadContent);
-  document.getElementById('list-files-button').onclick = withStore(listFiles);
-  document.getElementById('dl-button').onclick = withStore(downloadFile);
+  document.getElementById('file-button').onclick = function () {
+    applyTokenDo(uploadFile);
+  }
 
-  withStore(listFiles)();
+  document.getElementById('content-button').onclick = function () {
+    applyTokenDo(uploadContent);
+  }
+
+  document.getElementById('list-files-button').onclick = function () {
+    applyTokenDo(listFiles);
+  }
+
+  document.getElementById('dl-button').onclick = function () {
+    applyTokenDo(downloadFile);
+  }
+
+  applyTokenDo(listFiles);
 };
